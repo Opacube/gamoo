f76c3231f42e28e43bc3437ecc5847750656d246
Opacube <opacube@gmail.com>
2023-01-23 14:42:56 +0100

    ok

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-okss
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <div class="nav">
+        <nav>
+            <li><a href=""><i class="material-icons">person</i></a></li>
+            <li><a href=""><i class="material-icons">feed</i></a></li>
+            <li<a href=""><i class="material-icons">search</i></a></li>
+            <li><a href=""><i class="material-icons">chat_bubble</i></a></li>
+            <li><a href=""><i class="material-icons">groups</i></a></li>
+            <li><a href=""><i class="material-icons">settings</i></a></li>
+        </nav>
+    </div>
+
+</head>
+<body>
+</body>
+</html>

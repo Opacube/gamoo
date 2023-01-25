40bc378ec4b44cd4df88810c7827ad732c345f30
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2023-01-25 12:11:44 +0100

    BDD en ligne

--- account/config.php
+++ account/config.php
@@ -2,10 +2,22 @@
 session_start();
 
 $pdo = new PDO(
+    'mysql:host=sql11.freemysqlhosting.net;dbname=sql11592988;',
+    'sql11592988',
+    'hmSbAfx2rc',
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+?>
+
+<?php/*
+session_start();
+
+$pdo = new PDO(
     'mysql:host=localhost;dbname=gamoo;',
     'root',
     '',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-?>
+*/?>
